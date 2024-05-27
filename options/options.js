ed94224d475c7f331ac4710852de726e7726fdf6
Crowdbotics <team@crowdbotics.com>
2024-05-27 21:49:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "youtube-organizer-48128",
+  url: "https://youtube-organizer-48128.botics.co",
+  api: "https://youtube-organizer-48128.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
